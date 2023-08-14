ae9f363ddadbcc3812f718098e87487cae479e1c
fautaro <fautaro@gmail.com>
2023-08-14 20:17:02 -0300

    Borro models que no se usan

--- Admin-Dashboard/Models/Entities/Profesion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Admin_Dashboard.Models.Entities
-{
-    public class Profesion
-    {
-        public int Profesion_Id { get; set; }
-        public string? Nombre { get; set; }
-        public string? Rubro { get; set; }
-    }
-}
