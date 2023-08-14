7d63c7997fcc2dc7f3f7deb99a4790ca04b208be
fautaro <fautaro@gmail.com>
2023-08-14 20:31:59 -0300

    Agrego entity framework

--- Admin-Dashboard/Data/ApplicationDbContext.cs
+++ Admin-Dashboard/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Admin_Dashboard.Models.Entities;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Admin_Dashboard.Data
@@ -11,22 +10,11 @@ namespace Admin_Dashboard.Data
         {
         }
 
-        public DbSet<Turno> Turno { get; set; }
-
-        public DbSet<Profesional> Profesional { get; set; }
-        public DbSet<Profesion> Profesion { get; set; }
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Configuraciones adicionales de las tablas, relaciones, etc.
-            modelBuilder.Entity<Turno>()
-                .HasKey(e => e.Turno_Id);
-            modelBuilder.Entity<Profesional>()
-                .HasKey(e => e.Profesional_Id);
-            modelBuilder.Entity<Profesion>()
-                .HasKey(e => e.Profesion_Id);
+
         }
     }
 }
